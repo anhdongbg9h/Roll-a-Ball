639d6edc33b763474f8a3fc1313cf04e1cac8e80
Hải Đông Nguyễn <44259918+anhdongbg9h@users.noreply.github.com>
2020-02-28 21:19:33 +0700

    first commit

---
+++ Roll a ball/Assets/Scripts/PlayerControll.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControll : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    public Text countText;
+    public Text winText;
+    private float moveSpeed=10.0f;
+    private float turnSpeed= 10.0f;
+    public GameObject other;
+
+    private int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+
+
+        // Destroy gameObject
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Destroy(GetComponent<MeshRenderer>());
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Destroy(other);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        //rb.AddForce(movement * speed);
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * turnSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * turnSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count>=12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
---
+++ Roll a ball/Assets/Scripts/SwicthScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwicthScene : MonoBehaviour
+{
+    public void GoToMinigameScene()
+    {
+        //SceneManager.LoadScene("minigame");
+        Application.LoadLevel("New Scene");
+    }
+    public void GoToMinigame1Scene()
+    {
+        SceneManager.LoadScene("Minigame1");
+    }
+}
